db98e4381c1843976c6a0229842bb3daa6a9b996
jusgolds <jusgolds@gmail.com>
2018-02-06 20:49:12 -0700

    Add Leagues, Teams, User_Fan to db

--- fanswarm/settings.py
+++ fanswarm/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'fs.apps.FsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- fs/models.py
+++ fs/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Leagues(models.Model):
+    league_name = models.CharField(max_length=120)
+    abbreviation = models.CharField(max_length=32)
+    BASKETBALL = 'BB'
+    FOOTBALL = 'FB'
+    BASEBALL = 'BS'
+    HOCKEY = 'HY'
+    SOCCER = 'SC'
+    sport_choices = (
+        (BASKETBALL, 'Basketball'),
+        (FOOTBALL, 'Football'),
+        (BASEBALL, 'Baseball'),
+        (HOCKEY, 'Hockey'),
+        (SOCCER, 'Soccer'),
+    )
+    sport = models.CharField(max_length=2, choices=sport_choices, default=None)
+
+class Teams(models.Model):
+    team_name = models.CharField(max_length=120)
+    team_location_name = models.CharField(max_length=120)
+    team_metro = models.CharField(max_length=120)
+    team_league = models.ForeignKey(Leagues, on_delete=models.CASCADE, default=None)
+    #need to add fan_group
+
+class User_Fan(models.Model):
+    username = models.CharField(max_length=32)
+    name = models.CharField(max_length=70)
+    location = models.CharField(max_length=32)
+    #need to add password, member since, avatar, confirmed, fav teams
