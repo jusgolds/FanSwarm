18117ab888a2372dde507b2b977ce0de4f0677a0
jusgolds <jusgolds@gmail.com>
2018-02-10 15:59:27 -0600

    Add login view

--- fanswarm/urls.py
+++ fanswarm/urls.py
@@ -2,9 +2,9 @@ from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
-    path('user/', include('fs.urls')),
+    path('', include('fs.urls')),
+    path('login/', include('fs.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
 ]
 
 
--- fs/views.py
+++ fs/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Welcome to Fanswarm!")
+
+def login(request):
+    return HttpResponse("This is where someone would login.")
